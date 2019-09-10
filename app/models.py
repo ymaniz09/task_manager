a78b0b405bce0a3446ab0ada4c4e12b9e49711e6
Ygor <ygor.maniz@gmail.com>
2019-09-10 10:58:29 -0300

    Adding MySQL database and base html template

--- app/models.py
+++ app/models.py
@@ -1,3 +1,16 @@
 from django.db import models
 
 # Create your models here.
+
+
+class Tasks(models.Model):
+    PRIORITY_CHOICES = [
+        ("H", "HIGH"),
+        ("R", "REGULAR"),
+        ("L", "LOW"),
+    ]
+
+    title = models.CharField(max_length=150, null=False, blank=False)
+    description = models.CharField(max_length=300, null=False, blank=False)
+    expiration_date = models.DateField(null=False, blank=False)
+    priority = models.CharField(max_length=1, choices=PRIORITY_CHOICES, null=False, blank=False)
